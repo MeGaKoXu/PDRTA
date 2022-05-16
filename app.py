763377d5c4fb3ff1636d951cda98583047f21fd2
Krzysztof Majak <105597716+MeGaKoXu@users.noreply.github.com>
2022-05-16 14:06:22 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import numpy as np
 import pandas as pd
 
 
-# In[2]:
+# In[3]:
 
 
 from sklearn.datasets import load_iris
 iris = load_iris()
 
 
-# In[3]:
+# In[4]:
 
 
 df = pd.DataFrame(data = np.c_[iris['data'], iris['target']], 
@@ -23,21 +23,22 @@ df = pd.DataFrame(data = np.c_[iris['data'], iris['target']],
 df
 
 
-# In[4]:
+# In[5]:
 
 
 X = df.iloc[:, [0, 2]]
 y = df.iloc[:, [4]]
 
 
-# In[5]:
+# In[6]:
 
 
 from sklearn.model_selection import train_test_split
 from flask import Flask, request
+import pickle
 
 
-# In[6]:
+# In[7]:
 
 
 class Perceptron:
@@ -68,21 +69,21 @@ class Perceptron:
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
 
-# In[ ]:
+# In[9]:
 
 
 app = Flask(__name__)
 
+#127.0.0.1:5000/api/predict?sl=4.5&pl=3.2
 @app.route("/api/predict", methods=["GET"])
 def home():
-    iris = load_iris()
-    model = Perceptron()
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    model = Perceptron(0.01, 1000)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     X_train = X_train.to_numpy()
     y_train = y_train.to_numpy()
     model.fit(X_train, y_train)
-    sepal_length = 4.5
-    petal_length = 3.2
+    sepal_length = float(request.args.get('sl'))
+    petal_length = float(request.args.get('pl'))
     array = np.array([sepal_length, petal_length])
     prediction = model.predict(array)
     prediction = prediction.tolist()
@@ -96,10 +97,4 @@ def home():
     
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
-
-
-# In[ ]:
-
-
-
 
